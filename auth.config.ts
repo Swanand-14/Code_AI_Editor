514a1821a42f7ce4ada828cca7aed84007154749
Swanand Takalkar <takalkarswanand@gmail.com>
2025-10-01 08:52:03 +0530

    add middlewares,complete authentication

---
+++ auth.config.ts
@@ -0,0 +1,17 @@
+// auth.config.ts
+import type { NextAuthConfig } from "next-auth"
+import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
+
+export default {
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+} satisfies NextAuthConfig
