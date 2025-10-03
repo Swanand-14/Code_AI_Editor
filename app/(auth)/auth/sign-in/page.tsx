c65b79ea3e2adf94702dc9a9e1817da9ad5670c3
Swanand Takalkar <takalkarswanand@gmail.com>
2025-10-03 09:07:53 +0530

    feat(auth): implement sign-in page with image and form component
    - Updated sign-in page to include a login image and integrated SignInFormClient component for user authentication.
    - Enhanced RootLayout to provide session context using SessionProvider from next-auth.
    - Added UserButton component to display user information and logout functionality.
    - Created LogoutButton component for user logout action.
    - Developed SignInFormClient component for Google and GitHub sign-in options.
    - Implemented useCurrentUser hook to fetch current user session data.
    - Updated Prisma schema to reflect changes in user and session models.
    - Added login.svg asset for visual enhancement on the sign-in page.

--- app/(auth)/auth/sign-in/page.tsx
+++ app/(auth)/auth/sign-in/page.tsx
@@ -1,10 +1,15 @@
 "use client"
 
 import React from 'react'
+import Image from 'next/image'
+import SignInFormClient from '@/modules/auth/components/sign-in-form-client'
 
 function page() {
   return (
-    <div>Sign-in</div>
+    <>
+    <Image src={"/login.svg"} alt='login' height={300} width={300}/>
+    <SignInFormClient/>
+    </>
   )
 }
 
