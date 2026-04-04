f512dacb5ca2063b107c8cd49eb4cb05deb07e90
Swanand Takalkar <takalkarswanand@gmail.com>
2026-04-04 16:08:38 +0530

    tempmiddleware

--- middleware.ts
+++ middleware.ts
@@ -1,41 +1,48 @@
-import NextAuth from "next-auth";
-import {
-DEFAULT_LOGIN_REDIRECT,
-authRoutes,
-publicRoutes,protectedRoutes,
-apiAuthPrefix
-} from "@/routes"
-import authConfig from "./auth.config";
-const {auth} = NextAuth(authConfig)
+// import NextAuth from "next-auth";
+// import {
+// DEFAULT_LOGIN_REDIRECT,
+// authRoutes,
+// publicRoutes,protectedRoutes,
+// apiAuthPrefix
+// } from "@/routes"
+// import authConfig from "./auth.config";
+// const {auth} = NextAuth(authConfig)
 
-export default auth((req)=>{
-    const {nextUrl} = req;
-    const isLoggedIn = !!req.auth
-    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-    const isPublicRoutes = publicRoutes.includes(nextUrl.pathname)
-    const isAuthRoute = authRoutes.includes(nextUrl.pathname)
-    console.log({ 
-    path: nextUrl.pathname, 
-    isLoggedIn, 
-    isAuthRoute, 
-    isPublicRoutes 
-  });
+// export default auth((req)=>{
+//     const {nextUrl} = req;
+//     const isLoggedIn = !!req.auth
+//     const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+//     const isPublicRoutes = publicRoutes.includes(nextUrl.pathname)
+//     const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+//     console.log({ 
+//     path: nextUrl.pathname, 
+//     isLoggedIn, 
+//     isAuthRoute, 
+//     isPublicRoutes 
+//   });
 
-    if(isApiAuthRoute){
-        return null;
-    }
-    if(isAuthRoute){
-        if(isLoggedIn){
-            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT,nextUrl));
-        }
-        return null;
-    }
-    if(!isLoggedIn && !isPublicRoutes){
-        return Response.redirect(new URL("/auth/sign-in",nextUrl))
-    }
-    return null;
-});
+//     if(isApiAuthRoute){
+//         return null;
+//     }
+//     if(isAuthRoute){
+//         if(isLoggedIn){
+//             return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT,nextUrl));
+//         }
+//         return null;
+//     }
+//     if(!isLoggedIn && !isPublicRoutes){
+//         return Response.redirect(new URL("/auth/sign-in",nextUrl))
+//     }
+//     return null;
+// });
+
+// export const config = {
+//     matcher:["/((?!api|_next/static|_next/image|favicon.ico).*)",]
+// }
+export default function middleware() { 
+  return null 
+}
 
 export const config = {
-    matcher:["/((?!api|_next/static|_next/image|favicon.ico).*)",]
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)",]
 }
